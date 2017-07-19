9fab2aa9af7c9dc67595d08ecb43f5e33a8dbabe
Dan Bikle <danbikle@outlook.com>
2017-07-19 16:49:40 -0700

    more

--- gunicorn.bash
+++ gunicorn.bash
@@ -4,6 +4,6 @@
 
 # This script should start a gunicorn web server so I can see my django site.
 
-~/anaconda3/bin/gunicorn gettingstarted.wsgi
+~/anaconda3/bin/gunicorn --reload -b '0.0.0.0:16101' gettingstarted.wsgi
 
 exit
